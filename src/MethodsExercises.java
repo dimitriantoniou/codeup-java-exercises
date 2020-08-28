8f71671692ec3289869db4ae787ab3cadbd9996b
dimitriantoniou <dma2012@gmail.com>
2020-08-28 09:04:27 -0500

    methods workign

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -2,26 +2,24 @@ import java.util.Scanner;
 public class MethodsExercises {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-
-        public static double additionAB(double a, double b){
-            double sum = a + b;
+        public static int addition(int a, int b){
+            int sum = a + b;
             System.out.printf("%d + %d = %d%n", a, b, sum);
         }
-        public static double subtractionAB(int a, int b){
+        public static double subtraction(int a, int b){
             int sumSubtracted = a-b;
             System.out.printf("%d-%d=%d%n",a,b,sumSubtracted);
         }
-        public static void multiplicationAB(int a, int b){
+        public static void multiplication(int a, int b){
             int product = a * b;
             System.out.printf("%d * %d = %d%n",a,b,product);
         }
-        public static void divisionAB(int a, int b){
+        public static void division(int a, int b){
             int divided = a/b;
             System.out.printf("%d / %d = %d%n",a,b,divided);
         }
-        public static void modulusAB(int a, int b){
-            int modulus = a%b;
+        public static void modulus(int a, int b){
+            int modulused = a%b;
             System.out.printf("%d % %d = %d%n",a,b,modulus);
         }
     }
