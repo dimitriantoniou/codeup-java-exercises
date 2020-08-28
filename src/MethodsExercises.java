d6c8fde0b00b2b06c851b92294453681f61110c1
dimitriantoniou <dma2012@gmail.com>
2020-08-28 09:24:45 -0500

    methods exercise #2 in progress

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -3,24 +3,36 @@ public class MethodsExercises {
     public static int addition(int a, int b) {
         return a + b;
     }
+
     public static int subtraction(int a, int b) {
-        return a -b;
+        return a - b;
     }
+
     public static int multiplication(int a, int b) {
         return a * b;
     }
+
     public static int division(int a, int b) {
         return a / b;
     }
+
     public static int modulus(int a, int b) {
         return a % b;
     }
+    /*
+    public static void getInteger(int min, int max){
+        System.out.print("Enter a number between 1 and 10: ");
+        int userInput = getInteger(1,10);
+        return;
+    }
+     */
+
 
     public static void main(String[] args) {
-        System.out.println(addition(1,4));
-        System.out.println(subtraction(1,4));
-        System.out.println(multiplication(1,4));
-        System.out.println(division(1,4));
-        System.out.println(modulus(1,4));
+        System.out.println(addition(1, 4));
+        System.out.println(subtraction(1, 4));
+        System.out.println(multiplication(1, 4));
+        System.out.println(division(1, 4));
+        System.out.println(modulus(1, 4));
     }
 }
