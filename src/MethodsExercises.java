3857ccc7667f9603e94fef98a437d7f16116eea0
dimitriantoniou <dma2012@gmail.com>
2020-08-28 09:20:33 -0500

    completed methods exercise #1

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,26 +1,26 @@
-import java.util.Scanner;
 public class MethodsExercises {
-    public static void main(String[] args) {
 
-        public static int addition(int a, int b){
-            int sum = a + b;
-            System.out.printf("%d + %d = %d%n", a, b, sum);
-        }
-        public static double subtraction(int a, int b){
-            int sumSubtracted = a-b;
-            System.out.printf("%d-%d=%d%n",a,b,sumSubtracted);
-        }
-        public static void multiplication(int a, int b){
-            int product = a * b;
-            System.out.printf("%d * %d = %d%n",a,b,product);
-        }
-        public static void division(int a, int b){
-            int divided = a/b;
-            System.out.printf("%d / %d = %d%n",a,b,divided);
-        }
-        public static void modulus(int a, int b){
-            int modulused = a%b;
-            System.out.printf("%d % %d = %d%n",a,b,modulus);
-        }
+    public static int addition(int a, int b) {
+        return a + b;
+    }
+    public static int subtraction(int a, int b) {
+        return a -b;
+    }
+    public static int multiplication(int a, int b) {
+        return a * b;
+    }
+    public static int division(int a, int b) {
+        return a / b;
+    }
+    public static int modulus(int a, int b) {
+        return a % b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(addition(1,4));
+        System.out.println(subtraction(1,4));
+        System.out.println(multiplication(1,4));
+        System.out.println(division(1,4));
+        System.out.println(modulus(1,4));
     }
 }
