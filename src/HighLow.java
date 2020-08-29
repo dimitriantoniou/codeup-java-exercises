a5f5a1802d85e442017728ef4d725436ccfe4b59
dimitriantoniou <dma2012@gmail.com>
2020-08-29 08:40:02 -0500

    started HighLow game

---
+++ src/HighLow.java
@@ -0,0 +1,9 @@
+//Game picks a random number between 1 and 100.
+//Prompts user to guess the number.
+//All user inputs are validated.
+//If user's guess is less than the number, it outputs "HIGHER".
+//If user's guess is more than the number, it outputs "LOWER".
+//If a user guesses the number, the game should declare "GOOD GUESS!"
+
+public class HighLow {
+}
