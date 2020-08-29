75cf7d6ff0ead2e6e374d307f93aec5a8adc2457
dimitriantoniou <dma2012@gmail.com>
2020-08-29 08:28:28 -0500

    methods exercise 2 in progress

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class MethodsExercises {
 
     public static int addition(int a, int b) {
@@ -19,13 +21,10 @@ public class MethodsExercises {
     public static int modulus(int a, int b) {
         return a % b;
     }
-    /*
-    public static void getInteger(int min, int max){
-        System.out.print("Enter a number between 1 and 10: ");
-        int userInput = getInteger(1,10);
-        return;
-    }
-     */
+    //Create a method that validates that user input is in a certain range
+
+
+
 
 
     public static void main(String[] args) {
@@ -34,5 +33,35 @@ public class MethodsExercises {
         System.out.println(multiplication(1, 4));
         System.out.println(division(1, 4));
         System.out.println(modulus(1, 4));
+    }
+    public static int validateInteger(int min, int max){
+        System.out.print("Enter a number between 1 and 10: ");
+        Scanner sc = new Scanner (System.in);
+        int userInput = sc.nextInt();
+        if (userInput >=min && userInput <=max){
+            System.out.println("That works!");
+            return userInput;
+        }else{
+            System.out.println("That doens\'t work :( Please enter another number!");
+            return validateInteger(min, max);
+        }
+    }
+
+    public static void askNumber (){
+        Scanner scanner = new Scanner (System.in);
+        System.out.println(("Please enter a number between 1 and 10"));
+        int userInput = validateInteger (1,10);
+        long fact = userInput;
+        for (int i =1; i <userInput; i++){
+            fact = fact *i;
+        }
+        System.out.println(userInput + "! = " + fact);
+        System.out.println("Do you want to continue? Y/N");
+        String userContinue = scanner.next();
+        if(userContinue.equalsIgnoreCase("y")){
+            askNumber();
+        }else{
+            System.out.println("Bye bye!");
+        }
     }
 }
