f6f9c6653a27c6a984b9891bb7bf6a884014ef0f
dimitriantoniou <dma2012@gmail.com>
2020-08-29 21:10:32 -0500

    Circle.java in progress

---
+++ src/Shapes/Circle.java
@@ -0,0 +1,14 @@
+package Shapes;
+import util.Input;
+
+public class Circle {
+    private double radius;
+
+    public Circle(){
+        radius = Input.getDouble();
+        System.out.println("radius = " + this.radius);
+        System.out.println("getArea() = "+ getArea());
+        System.out.printf("Your circumference = " + getCircumference() +"%n");
+    }
+
+}
