2c9a8264e13718700768dc8e3c51751ee7098aae
dimitriantoniou <dma2012@gmail.com>
2020-08-29 20:46:03 -0500

    OOP exercises in progress

--- src/Person.java
+++ src/Person.java
@@ -19,7 +19,24 @@ public class Person {
         sayHello();
     }
     public static void main(String[] args){
-        Person dimitri = new Person("Dimitri");
-        System.out.println(dimitri.getName());
+        //Person dimitri = new Person("Dimitri");
+        //System.out.println(dimitri.getName());
+
+        //Person person1 = new Person("John");
+        //Person person2 = new Person("John");
+        //System.out.println(person1.getName().equals(person2.getName()));
+        //System.out.println(person1 == person2);
+
+        //Person person1 = new Person("John");
+        //Person person2 = person1;
+        //System.out.println(person1 == person2);
+
+        Person person1 = new Person("John");
+        Person person2 = person1;
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+        person2.setName("Jane");
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
     }
 }
