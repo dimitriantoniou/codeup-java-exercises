0eaa4cba9313b6a104e5c4d7905c1af893fcf62d
dimitriantoniou <dma2012@gmail.com>
2020-08-31 07:36:27 -0500

    javai arays exercises in progress

---
+++ src/ArraysExercises.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class ArraysExercises {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(numbers));
+    }
+
+    // int [] numbers = {1, 2, 3, 4, 5};
+    // System.out.println(Arrays.toString(numbers));
+    // System.out.println(numbers);
+    Person person1 = new Person("dimitr");
+    Person person2 = new Person("bernice");
+    Person person3 = new Person("caitlin");
+
+    Person [] peopleArray = {person1, person2, person3};
+
+}
+
