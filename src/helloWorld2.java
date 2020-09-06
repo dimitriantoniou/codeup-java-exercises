9291cb315f677b366486aac30579419946473545
Dimitri <dma2012@gmail.com>
2020-09-06 07:43:05 -0500

    completed syntax, types, and variables exercises

--- src/helloWorld2.java
+++ src/helloWorld2.java
@@ -1,5 +1,79 @@
 public class helloWorld2 {
         public static void main(String[] args) {
-            System.out.println("Hello, World!");
+
+            //System.out.println("Hello, World!");
+
+            //int myFavoriteNumber = 22;
+            //System.out.println(myFavoriteNumber);
+
+            //String myString = "22";
+            //System.out.println(myString);
+
+            //this will break with a character instead of a string
+            //String myStringChar = '22';
+            //System.out.println(myStringChar);
+
+            //won't run because of the wrong data type
+            //String myString = 3.14159;
+            //System.out.println(myString);
+
+            //won't print because it has not been initialized
+            //long myNumber;
+            //System.out.println(myNumber);
+
+            //wrong data type
+            //long myNumber = 3.14;
+            //System.out.println(myNumber);
+
+            //L does not print out
+            //long myNumber = 123L;
+            //System.out.println(myNumber);
+
+            //this does print
+            //long myNumber=123;
+            //System.out.println(myNumber);
+
+            //this will not print
+            //float myNumber = 3.14;
+            //System.out.println(myNumber);
+
+            /*int x = 5;
+            System.out.println(x++);
+            System.out.println(x);
+
+            int x = 5;
+            System.out.println(++x);
+            System.out.println(x);
+
+            //code output is the same
+            //first increments before hand, then uses the variable in the expression
+            //in the second, variable is used and then incremented
+             */
+
+            //camt use class
+            //String class="class";
+            //System.out.println(class);
+
+            //neither code works
+            //String theNumberThree = "three";
+            //Object o = theNumberThree;
+            //int three = (int) o;
+            //casting error
+            //int three = (int) "three";
+            //converting error
+
+            //int x = 4;
+            //x = x + 5;
+            //x +=5;
+
+            //int x = 3;
+            //int y = 4;
+            //y = y * x;
+            //y*=x;
+
+            //int x = 10;
+            //int y = 2;
+            //x /=y;
+            //y /= x;
         }
 }
