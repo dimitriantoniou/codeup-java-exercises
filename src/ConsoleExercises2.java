530d8fb0e9924b9e7f6df5f77299ec69793e2f4f
Dimitri <dma2012@gmail.com>
2020-09-06 08:12:29 -0500

    completed console exercises again

--- src/ConsoleExercises2.java
+++ src/ConsoleExercises2.java
@@ -6,6 +6,8 @@ public class ConsoleExercises2 {
         //System.out.println("The value of pi is approximately 3.14.");
         //System.out.format("The value of pi is approximately %.2f%n.",pi);
 
+        /*
+
         Scanner sc = new Scanner (System.in);
         System.out.println("Please enter a number!");
         int userNumber = sc.nextInt();
@@ -19,5 +21,15 @@ public class ConsoleExercises2 {
         System.out.println("Please give me a random word! ");
         String wordThree = sc2.next();
         System.out.printf("%s%n%s%n%s%n",wordOne,wordTwo,wordThree);
+
+         */
+
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("In feet, How long is Codeup\'s classroom?");
+        int length = Integer.parseInt(scanner.nextLine());
+        System.out.println("In feet, how wide is Codeup\'s classroom?");
+        int width = Integer.parseInt(scanner.nextLine());
+        int area = length*width;
+        System.out.printf("With a length of %s and a width of %s, the classroom's area is %s",length, width, area);
     }
 }
