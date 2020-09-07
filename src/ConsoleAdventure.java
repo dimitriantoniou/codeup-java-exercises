86178c9d84c5cf7aa92c43cc6ab581cb07ea2651
Dimitri <dma2012@gmail.com>
2020-09-07 18:42:27 -0500

    started console adventure game

---
+++ src/ConsoleAdventure.java
@@ -0,0 +1,21 @@
+public class ConsoleAdventure {
+}
+
+//Ask if the user is ready to start. If they type in "yes", start the game.
+//Ask the user for their name. Store this as a variable to personalize the adventure.
+//A classic RPG will have the hero going through different scenarios and battling their enemies.
+//Display enemy stats and hero stats. Properties and values can be assigned by you.
+//For example: Health, Attack Points, etc.
+//Decide on what actions your hero can take.
+//Attack (decreases enemyHealth)
+//Drink potion (adds to heroHealth)
+//Run? (ends the game)
+//Keep asking for user input until the enemyHealth reaches 0, then end the game.
+//Bonuses
+//
+//Allow the user to specify hero stats and enemy stats.
+//Automate an enemyAttack after each time the hero takes a turn.
+//End the game if either heroHealth or enemyHealth drops to 0.
+//Indicate how many potions your hero starts with. Decrement this number each time 'Use Potion' action is used.
+//
+//Remember that these are only suggestions. Feel free to get creative and have fun!!
