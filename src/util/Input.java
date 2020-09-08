934874990beb514d453dcb81151e97dd330b38a0
Dimitri <dma2012@gmail.com>
2020-09-08 07:47:23 -0500

    refactored methods

--- src/util/Input.java
+++ src/util/Input.java
@@ -9,36 +9,43 @@ public class Input {
         return newString;
     }
 
-    public boolean yesNo(){
+    public void yesNo() {
         System.out.println("Would you like to continue? y/n");
         String userInput = sc.nextLine();
         if (userInput.equalsIgnoreCase("y") || (userInput.equalsIgnoreCase("yes"))) {
             System.out.println("True");
-        }else{
+        } else {
             System.out.println("False");
+        }
     }
 
-    public int getInt(int min, int max){
+    public int getInt(int min, int max){//syntax error expecting a ; here
         System.out.printf("Please give an integer between %d and %d",min,max);
         int input = Integer.parseInt(sc.nextLine());
         if(input>min && input<max) {
-            System.out.printf("Please try again!");
-        }else{
             System.out.printf("%d works",sc.nextInt());
-            return input;
+        }else{
+            System.out.printf("Please try again!");
     }
 
-    public int getInt(){
-
-    }
+    //public int getInt(){
+      //  int input = Integer.parseInt(sc.nextLine());
+        //return input;
+    //}
 
     public double getDouble(double min, double max){
         System.out.printf("Please enter a double between %d and %d. Hint: use a decimal!",min,max)
-            return ;
+            double inputDouble = Double.parseDouble(sc.nextLine());
+        if (inputDouble>min && inputDouble < max){
+            System.out.printf("Geat, %d is between %d and %d.",inputDouble,min,max);
+        }else{
+            System.out.println("Sorry, please try again.");
+        }
     }
 
-    public double getDouble(){
-        return ;
-    }
+   // public double getDouble(){
+      //  return ;
+    //}
 
+}
 }
