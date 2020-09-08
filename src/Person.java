fb204f6a5c55dcdc070323707a0ef6a6364be54d
Dimitri <dma2012@gmail.com>
2020-09-08 07:08:52 -0500

    OOP exercises

--- src/Person.java
+++ src/Person.java
@@ -1,42 +1,40 @@
 public class Person {
-    private String firstName;
-    public String getName(){
-//TODO: return the person's name
-        return this.firstName;
+    private String name;
+
+    public String getName(){//TODO: return the person's name
+        return this.name;
     }
 
-    public void setName(String setName){
-//TODO: change the name property to the passed value
-        this.firstName = setName;
+    public void setName(String setName){//TODO: change the name property to the passed value
+        this.name = setName;
     }
-    public void sayHello(){
-//TODO: print a message to the console using the person's name
-        System.out.printf("Hello %s!",this.firstName);
+
+    public void sayHello(){//TODO: print a message to the console using the person's name
+        System.out.printf("Hello %s!",this.name);
     }
-    public Person(String usersName){
-        setName(usersName);
-        System.out.println("Person's name is: " + getName());
-        sayHello();
+
+    public Person(String name){
+        this.name=name;
     }
+
+    /*understanding references
+    Person person1 = new Person("John");
+    Person person2 = new Person("John");
+    Person person1 = new Person("John");
+    Person person2 = person1;
+    */
+
     public static void main(String[] args){
-        //Person dimitri = new Person("Dimitri");
-        //System.out.println(dimitri.getName());
+        Person dimitri = new Person("Dimitri");
+        System.out.print(dimitri.getName());
+        dimitri.setName("Dimitri Antoniou");
+        System.out.print(dimitri.getName());
+        dimitri.sayHello();
+/*
+        System.out.println(person1.getName().equals(person2.getName()));
+        System.out.println(person1 == person2);
+                System.out.println(person1 == person2);
+*/
 
-        //Person person1 = new Person("John");
-        //Person person2 = new Person("John");
-        //System.out.println(person1.getName().equals(person2.getName()));
-        //System.out.println(person1 == person2);
-
-        //Person person1 = new Person("John");
-        //Person person2 = person1;
-        //System.out.println(person1 == person2);
-
-        Person person1 = new Person("John");
-        Person person2 = person1;
-        System.out.println(person1.getName());
-        System.out.println(person2.getName());
-        person2.setName("Jane");
-        System.out.println(person1.getName());
-        System.out.println(person2.getName());
     }
 }
