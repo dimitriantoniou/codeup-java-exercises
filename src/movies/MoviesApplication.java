414d0d982a14576114ec9829b44a2b20ac0978d8
Dimitri <dma2012@gmail.com>
2020-09-13 06:42:44 -0400

    instructions

--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -7,5 +7,13 @@ public class MoviesApplication {
         //view all movies
 
         //view movies by category
+
+        // Use your Input class to get input from the user, and display information based on their choice.
+        // If a category is selected, only movies from that category should be displayed.
+        //Your application should continue to run until the user chooses to exit.
+
+        // Bonus: Add functionality to allow a user to add a new movie to the list.
+
+
     }
 }
