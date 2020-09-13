b6b8dd37d0aaeb2d61a056b89e3e685c7613ad27
Dimitri <dma2012@gmail.com>
2020-09-13 06:41:11 -0400

    commented in instructions

--- src/movies/Movie.java
+++ src/movies/Movie.java
@@ -2,19 +2,21 @@ package movies;
 
 public class Movie {
 
+    //    private fields for name and category
     private String name;
     private String category;
 
+    //  constructor that sets name/category
     public Movie (String name, String category){
         setName(name);
         setCategory(category);
     }
 
+    //    Create methods to access these properties and change them (getters and setters).
     public void setName(String setname){this.name = setname;}
     public void setCategory(String setcategory){this.category = setcategory;}
 
     public static String getName(Movie element){return element.name;}
     public static String getCategory(Movie element){return element.category;}
-
 
 }
--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -1,6 +1,7 @@
 package movies;
 import util.Input;
 
+//    Create a class named MoviesApplication that has a main method.
 public class MoviesApplication {
     public static void main (String[] args){
         //view all movies
