8da7201d9dc296ee1d4cc3637838488441c39f8b
Dimitri <dma2012@gmail.com>
2020-09-14 18:30:52 -0400

    super side

--- src/Shapes/Square.java
+++ src/Shapes/Square.java
@@ -3,9 +3,12 @@ package Shapes;
 public class Square extends Rectangle {
     //define a constructor that accepts one argument, side
     //call parent constructor to set both length and width = side
+
+    private int side;
+
     public Square(double side){
-        length = side;
-        width=side;
+        super(side,side);
+        this.side=side;
     }
     //override the getArea and getPerimeter methods with the following definitions for a square
     //perimeter = 4 x side
