ea138b37bab88e2146bbdb36bc23778b9ab86f4a
Dimitri <dma2012@gmail.com>
2020-09-15 07:50:19 -0400

    abstract class quadrilateral

---
+++ src/Shapes/Quadrilateral.java
@@ -0,0 +1,25 @@
+package Shapes;
+
+public abstract class Quadrilateral extends Shape implements Measurable{
+    protected double length;
+    protected double width;
+
+    public Quadrilateral(double length, double width){
+        this.length=length;
+        this.width=width;
+    }
+
+    public double getArea(){    //create a getArea method
+        return length*width;
+    }
+    public double getPerimeter(){    //create a getPerimeter method
+        return 2*(length+width);
+    }
+
+    public void setLength(double length){
+        this.length=length;
+    }
+    public void setWidth(double width){
+        this.width=width;
+
+    }
