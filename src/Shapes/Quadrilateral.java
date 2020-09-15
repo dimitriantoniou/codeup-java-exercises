f5cbe7eff1dcda272a0039d18757b4b0630168e4
Dimitri <dma2012@gmail.com>
2020-09-15 07:51:00 -0400

    closed curly brace

--- src/Shapes/Quadrilateral.java
+++ src/Shapes/Quadrilateral.java
@@ -1,25 +1,28 @@
 package Shapes;
 
-public abstract class Quadrilateral extends Shape implements Measurable{
+public abstract class Quadrilateral extends Shape implements Measurable {
     protected double length;
     protected double width;
 
-    public Quadrilateral(double length, double width){
-        this.length=length;
-        this.width=width;
+    public Quadrilateral(double length, double width) {
+        this.length = length;
+        this.width = width;
     }
 
-    public double getArea(){    //create a getArea method
-        return length*width;
-    }
-    public double getPerimeter(){    //create a getPerimeter method
-        return 2*(length+width);
+    public double getArea() {    //create a getArea method
+        return length * width;
     }
 
-    public void setLength(double length){
-        this.length=length;
+    public double getPerimeter() {    //create a getPerimeter method
+        return 2 * (length + width);
     }
-    public void setWidth(double width){
-        this.width=width;
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
 
     }
+}
