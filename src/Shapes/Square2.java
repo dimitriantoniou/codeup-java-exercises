91e79d0c52d8c8094e5d4d88258254c0bb8041a5
Dimitri <dma2012@gmail.com>
2020-09-15 07:46:14 -0400

    duplicate square and rectangle to save 1st code

---
+++ src/Shapes/Square2.java
@@ -0,0 +1,18 @@
+package Shapes;
+
+public class Square2 extends Rectangle {
+
+    public Square2(double side){    //define a constructor that accepts one argument, side
+        super(side,side);    //call parent constructor to set both length and width = side
+    }
+
+    //override the getArea and getPerimeter methods with the following definitions for a square
+    public double getArea(){    //area = side ^ 2
+        return super.width*super.width;
+    }
+
+    public double getPerimeter(){   //perimeter = 4 x side
+        return 4*super.width;
+    }
+
+}
