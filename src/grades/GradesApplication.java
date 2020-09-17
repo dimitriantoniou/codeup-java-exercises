ec8a5b2ec7ac62d97d85b1a73980c05337ddc5d4
Dimitri <dma2012@gmail.com>
2020-09-17 07:00:32 -0400

    working on command line interface collections exercise

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -6,11 +6,17 @@ public class GradesApplication {
     public static void main(String[] args){
         HashMap<String, Student> students = new HashMap<>();
 
-        Student ryan = new Student ("Ryan");
-        Student zach=new Student("Zach");
-        Student fer=new Student("Fer");
-        Student justin =new Student("Justin");
+        Student ryan = new Student ("ryan");
+        Student zach=new Student("zach");
+        Student fer=new Student("fer");
+        Student justin =new Student("justin");
 
+        students.put("ryanorsinger",ryan);
+        students.put("zgulde",zach);
+        students.put("fmendozaro",fer);
+        students.put("jreich5",justin);
+
+        /*
         ryan.addGrade(90);
         ryan.addGrade(50);
         ryan.addGrade(100);
@@ -23,17 +29,16 @@ public class GradesApplication {
         justin.addGrade(80);
         justin.addGrade(50);
         justin.addGrade(70);
+        */
 
-        students.put("ryanorsinger",ryan);
-        students.put("zgulde",zach);
-        students.put("fmendozaro",fer);
-        students.put("jreich5",justin);
 
+        /*
         System.out.println(ryan.getName()+ " grade average is "+ryan.getGradeAverage());
         System.out.println(zach.getName()+": grade average is "+zach.getGradeAverage());
         System.out.println(fer.getName()+": grade average is "+fer.getGradeAverage());
         System.out.println(justin.getName()+": grade average is "+justin.getGradeAverage());
+        */
 
-
+        System.out.println(students);
     }
 }
