e83397e35a1d43373a727ca7d49f188acaf8480e
Dimitri <dma2012@gmail.com>
2020-09-17 06:44:43 -0400

    added tests for student class

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -6,25 +6,35 @@ public class Student {
     private String name;
     private ArrayList<Integer> grades;
 
-    public Student (String name){
-        this.name=name;
-        this.grades=new ArrayList<>();
+    public Student(String name) {
+        this.name = name;
+        this.grades = new ArrayList<>();
     }
-    public String getName(){
+
+    public String getName() {
         System.out.println(name);
         return name;
     }
-    public void addGrade(int grade){
+
+    public void addGrade(int grade) {
         grades.add(grade);
     }
-    public double getGradeAverage(){
-        int sum =0;
-        for (int i=0;i<grades.size();i++){
-            sum+=grades[i];
+
+    public double getGradeAverage() {
+        int sum = 0;
+        for (int i = 0; i < grades.size(); i++) {
+            sum += grades.get(i);
         }
-        int average = sum/grades.size();
+        int average = sum / grades.size();
         return average;
     }
 
-}
+    public static void main(String[] args) {
+        Student dimitri = new Student("Dimitri");
+        dimitri.addGrade(90);
+        dimitri.addGrade(80);
+        dimitri.addGrade(70);
+        System.out.println("Dimitri's grade average is " + dimitri.getGradeAverage());
 
+    }
+}
