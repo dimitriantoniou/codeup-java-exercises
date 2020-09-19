87737005e14833a0ef8a49a678028a073797a4f2
Dimitri <dma2012@gmail.com>
2020-09-19 18:47:55 -0400

    added attendance hashmap

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -1,14 +1,17 @@
 package grades;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Student {
     private String name;
     private ArrayList<Integer> grades;
+    private HashMap<String,String>attendance;
 
     public Student(String name) {
         this.name = name;
         this.grades = new ArrayList<>();
+        this.attendance=new HashMap<>();
     }
 
     public String getName() {
@@ -27,6 +30,10 @@ public class Student {
         }
         int average = sum / grades.size();
         return average;
+    }
+
+    public void recordAttendance(String date, String value){
+
     }
 
     public static void main(String[] args) {
