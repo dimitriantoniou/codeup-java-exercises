d12bc8e7b5cdc01ffe3072af516730c13350de2f
Dimitri <dma2012@gmail.com>
2020-09-19 07:55:21 -0400

    while loop for continued application; search again;

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -6,18 +6,18 @@ import java.util.Scanner;
 import util.Input;
 
 public class GradesApplication {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         HashMap<String, Student> students = new HashMap<>();
 
-        Student ryan = new Student ("ryan");
-        Student zach=new Student("zach");
-        Student fer=new Student("fer");
-        Student justin =new Student("justin");
+        Student ryan = new Student("ryan");
+        Student zach = new Student("zach");
+        Student fer = new Student("fer");
+        Student justin = new Student("justin");
 
-        students.put("ryanorsinger",ryan);
-        students.put("zgulde",zach);
-        students.put("fmendozaro",fer);
-        students.put("jreich5",justin);
+        students.put("ryanorsinger", ryan);
+        students.put("zgulde", zach);
+        students.put("fmendozaro", fer);
+        students.put("jreich5", justin);
 
 
         ryan.addGrade(90);
@@ -40,26 +40,38 @@ public class GradesApplication {
         System.out.println(justin.getName()+": grade average is "+justin.getGradeAverage());
         */
 
-        //ask the user which student they would like to see more information about.
-        // The user should enter a GitHub username (i.e. one of the keys in your map).
-        // If the given input does not match up with a key in your map, tell the user that no users with that username were found.
-        // If the given username does exist, display information about that student, including their name and their grades.
-        //After the information is displayed, the application should ask the user if they want to continue,
-        // and keep running so long as the answer is yes.
 
         boolean continueApplication = true;
+        double gradeAverage = 0;
 
-        //while (continueApplication){
+        while (continueApplication) {
             System.out.println("Welcome! Here are our student github usernames:");
-        for (String key: students.keySet()){
-            System.out.println(key + " "); //Print the list of GitHub usernames out to the console,
+            for (String key : students.keySet()) {
+                System.out.println(key + " "); //Print the list of GitHub usernames out to the console,
+            }
+            System.out.println("What student would you like to select?"); //ask the user which student they would like to see more information about.
+
+            Scanner sc = new Scanner(System.in);
+            var userInput = sc.next(); // The user should enter a GitHub username
+            //System.out.println("You have selected: "+userInput); //verifies variable was created
+            while (!students.containsKey(userInput)) {
+                System.out.println("Sorry, that user does not match. Please enter another username.");        // If the given input does not match up with a key in your map, tell the user that no users with that username were found.
+                userInput = sc.next();
+            }
+            gradeAverage = students.get(userInput).getGradeAverage();        // If the given username does exist, display information about that student, including their name and their grades.
+            System.out.printf("Student: %s | Grades: %s", userInput, gradeAverage);
+
+            //After the information is displayed, the application should ask the user if they want to continue,
+            // and keep running so long as the answer is yes.
+            System.out.print("Would you like to search again? y/n");
+            System.out.println("");
+            Scanner scanner = new Scanner(System.in);
+            if (scanner.next().equalsIgnoreCase("y")) {
+                continueApplication = true;
+            }
+            System.out.print("Thank you for accessing the grades portal!");
         }
-        System.out.println("What student would you like to select?");
-        Scanner sc = new Scanner(System.in);
-        var userInput = sc.next();
-        System.out.println("You have selected: "+userInput);
-    }
-        //}
-     //   continueApplication = Input.yesNo("Would you like to search again? (y/n)");
+
 
     }
+}
