aeabe00089efb93184dd7ce3b8f73750894ca542
Dimitri <dma2012@gmail.com>
2020-09-20 07:49:50 -0400

    groceries class started

---
+++ src/Groceries.java
@@ -0,0 +1,22 @@
+public class Groceries {
+    //Create a grocery list command line application. You will use your input class from previous exercises.
+    //
+    //A user should be prompted if they would like to create a grocery list.
+    //
+    //If they pick yes, they will be prompted if they would like to enter a new item.
+    //
+    //If the users picks yes, they should be given the following three prompts:
+    //
+    //Given an ordered list of grocery categories to choose from, select the category.
+    //Enter name of the item.
+    //Enter how many of the item.
+    //The user will then be given the choice to finalize the list or add an additional item.
+    //
+    //Once the user finalizes the list, they will be presented with a complete list organized alphabetically and grouped by category, and including quantity.
+    //
+    //For an extra bonus, allow users to filter the results by only one category of items at a time.
+    //
+    //For a super gold star bonus, allow users to edit the list items.
+    //
+    //Consider before coding how the data should be organized. Using ArrayLists and HashMaps will be helpful.
+}
